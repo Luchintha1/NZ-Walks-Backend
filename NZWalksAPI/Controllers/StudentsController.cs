08bf200d189977f06ebb7edd00266104f1bc651d
Luchintha Anjana <luchinthaa@gmail.com>
2025-07-25 11:04:46 +0530

    Initial Commit - Setup Entity Framework and Database

---
+++ NZWalksAPI/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksAPI.Controllers
+{
+    // http://localhost:portNumber/api/Students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // http://localhost:portNumber/api/Students
+        [HttpGet]
+        public IActionResult GetAllStudentDetails()
+        {
+            string[] studentNames = new string[] { "John", "Elon", "James", "Emma", "Alexa" };
+            return Ok(studentNames);
+        }
+    }
+}
