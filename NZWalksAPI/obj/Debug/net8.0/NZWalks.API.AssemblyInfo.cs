d500c866976db5be79cc25031460ff355c15d0fa
Luchintha Anjana <luchinthaa@gmail.com>
2025-09-01 19:47:55 +0530

    All Completed with RestAPI

--- NZWalksAPI/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
+++ NZWalksAPI/obj/Debug/net8.0/NZWalks.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40d0f1fdd726da3ee7e270b8dd48295bc6a082a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+963a1bcf88ec41293ea1f8b81fd6f1b51e7dcef8")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalks.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
